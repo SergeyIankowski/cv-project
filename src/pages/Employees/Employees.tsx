d9a38971a75d98424c49bef3074a630db65fb928
SeregeyIankowski <garret933@gmail.com>
2023-04-24 15:48:11 +0300

    feat: add PageLayout component to Employees page

--- src/pages/Employees/Employees.tsx
+++ src/pages/Employees/Employees.tsx
@@ -1,6 +1,18 @@
 import {PageLayout} from "@/components/view/PageLayout/PageLayout";
+import {BreadCrumbsData} from "@/models/BreadCrumbsData.type";
+import HomeIcon from "@mui/icons-material/Home";
+import {Pages} from "@/models/Pages";
 import {FC} from "react";
 
+const breadCrumbs: BreadCrumbsData = [
+  {
+    text: "Home",
+    path: Pages.main,
+    icon: <HomeIcon fontSize="small" />,
+  },
+  {text: "Login", path: Pages.login},
+];
+
 export const Employees: FC = () => {
-  return <PageLayout>Some Children</PageLayout>;
+  return <PageLayout linksData={breadCrumbs}>Some Children</PageLayout>;
 };
