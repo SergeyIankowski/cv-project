8d320628bc61ee8b70c929922071070d159f196a
SeregeyIankowski <garret933@gmail.com>
2023-04-24 11:32:47 +0300

    fix: add BrowserRouter in index.tsx and installed dependesies to package.json

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,13 +1,16 @@
-import {FC} from "react";
-import {Route, Routes} from "react-router-dom";
 import {Pages} from "@/models/Pages";
+import {Page404} from "@/pages/Page404/Page404";
+import {Signup} from "./pages/Signup/Signup";
+import {Route, Routes} from "react-router-dom";
+import {Employees} from "./pages/Employees/Employees";
 
-export const AppRouter: FC = () => {
+export const AppRouter = () => {
   return (
     <Routes>
-      <Route path={Pages.main} />
-      <Route path={Pages.login} />
-      <Route path={Pages.employees} />
+      <Route path={Pages.main} element={<Employees />} />
+      <Route path={Pages.login} element={<Signup />} />
+      <Route path={Pages.employees} element={<Employees />} />
+      <Route path={Pages.notFound} element={<Page404 />} />
     </Routes>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
 import {createRoot} from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
 import {AppRouter} from "./AppRouter";
 import "./scss/main.scss";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <AppRouter />
-  </React.StrictMode>
+  </BrowserRouter>
 );
