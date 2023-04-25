70ea4e2a47a17704cce8ccf1053a1d8b654a84ba
SeregeyIankowski <garret933@gmail.com>
2023-04-25 09:48:46 +0300

    feat: add SignUp Route to AppRouter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,14 +1,16 @@
 import {Pages} from "@/models/Pages";
 import {Page404} from "@/pages/Page404/Page404";
-import {Signup} from "./pages/Signup/Signup";
+import {SignIn} from "@/pages/SignIn/SignIn";
 import {Route, Routes} from "react-router-dom";
+import {SignUp} from "./pages/SignUp/SignUp";
 import {Employees} from "./pages/Employees/Employees";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path={Pages.main} element={<Employees />} />
-      <Route path={Pages.login} element={<Signup />} />
+      <Route path={Pages.login} element={<SignIn />} />
+      <Route path={Pages.signup} element={<SignUp />} />
       <Route path={Pages.employees} element={<Employees />} />
       <Route path={Pages.notFound} element={<Page404 />} />
     </Routes>
