912591327895740b3f8e8def512e18c8d4a9cb36
SeregeyIankowski <garret933@gmail.com>
2023-04-29 13:47:32 +0300

    feat: implement sorting functions for table sorting

---
+++ src/utils/getComparator.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {Order} from "@/models/Order.type";
+import {descendingComparator} from "./descendingComparator";
+
+export function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (a: {[key in Key]: any}, b: {[key in Key]: any}) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
