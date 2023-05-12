c405e102a10e52ba526911d236a6e2cd9fc148ab
SeregeyIankowski <garret933@gmail.com>
2023-05-12 14:36:20 +0300

    feat: add root Authentication and nested forms to AppRouter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,26 +1,30 @@
 import {Pages} from "@/models/Pages";
 import {Page404} from "@/pages/Page404/Page404";
-import {SignIn} from "@/pages/SignIn/SignIn";
 import {Navigate, Route, Routes} from "react-router-dom";
-import {SignUp} from "./pages/SignUp/SignUp";
-import {Employees} from "./pages/Employees/Employees";
-import {Profile} from "./pages/Profile/Profile";
-import {ProfileForm} from "./components/containers/ProfileForm/ProfileForm";
+import {Employees} from "@/pages/Employees/Employees";
+import {Profile} from "@/pages/Profile/Profile";
+import {ProfileForm} from "@containers/ProfileForm/ProfileForm";
+import {SignInForm} from "@containers/SignInForm/SignInForm";
+import {Authentication} from "@/pages/Authentication/Authentication";
+import {SignUpForm} from "@containers/SignUpForm/SignUpForm";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path={Pages.main} element={<Navigate to={Pages.employees} />} />
-      <Route path={Pages.login} element={<SignIn />} />
-      <Route path={Pages.signup} element={<SignUp />} />
       <Route path={Pages.employees} element={<Employees />} />
       <Route path={Pages.notFound} element={<Page404 />} />
       <Route path={Pages.info.root} element={<Profile />}>
-        <Route index element={<Profile />}></Route>
+        <Route index element={<Navigate to={Pages.info.profile} />}></Route>
         <Route path={Pages.info.profile} element={<ProfileForm />} />
         <Route path={Pages.info.skills} element={<></>} />
         <Route path={Pages.info.languages} element={<></>} />
         <Route path={Pages.info.cvs} element={<></>} />
+      </Route>
+      <Route path={Pages.auth.root} element={<Authentication />}>
+        <Route index element={<Navigate to={Pages.auth.login} />} />
+        <Route path={Pages.auth.login} element={<SignInForm />} />
+        <Route path={Pages.auth.signup} element={<SignUpForm />} />
       </Route>
     </Routes>
   );
