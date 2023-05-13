11d5637196c663f6e6208a2b36f0b6c31335f730
SeregeyIankowski <garret933@gmail.com>
2023-05-13 15:06:10 +0300

    feat: add validation to SignIn form

--- src/components/containers/SignInForm/SignInForm.tsx
+++ src/components/containers/SignInForm/SignInForm.tsx
@@ -12,7 +12,8 @@ import {
   PasswordIconStyle,
 } from "@/components/view/MuiPagesStyles";
 import {Button} from "@containers/Button";
-import FormControl from "@mui/material/FormControl";
+import {useForm} from "react-hook-form";
+import {AuthValues} from "@/models/AuthValues.type";
 
 export const SignInForm = () => {
   const [
@@ -21,37 +22,72 @@ export const SignInForm = () => {
     setPasswordVisibilityFalse,
   ] = useBooleanState(false);
 
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<AuthValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: AuthValues) => {
+    console.log(data);
+  };
   return (
-    <FormControl sx={ContentStyle}>
-      <TypographyH3 text="Welcome Back" />
-      <TypographyH6 text="Hello Again! Sign up to continue!" />
-      <Input margin="dense" id="email" label="Email" />
-      <Box sx={{position: "relative"}}>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box sx={ContentStyle}>
+        <TypographyH3 text="Welcome Back" />
+        <TypographyH6 text="Hello Again! Sign up to continue!" />
         <Input
-          sx={{width: "100%"}}
           margin="dense"
-          id="password"
-          label="Password"
-          type={passwordVisibility ? "text" : "password"}
+          id="email"
+          label="Email"
+          name="email"
+          rules={{required: true, pattern: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i}}
+          error={errors.email ? true : false}
+          helperText={errors.email && "email is not correct"}
+          control={control}
         />
-        {passwordVisibility ? (
-          <VisibilityOffIcon
-            sx={PasswordIconStyle}
-            onClick={setPasswordVisibilityFalse}
+        <Box sx={{position: "relative"}}>
+          <Input
+            sx={{width: "100%"}}
+            margin="dense"
+            id="password"
+            label="Password"
+            type={passwordVisibility ? "text" : "password"}
+            name="password"
+            control={control}
+            rules={{
+              required: true,
+              minLength: 8,
+            }}
+            error={errors.password ? true : false}
+            helperText={
+              errors.password && "password must be at least 8 characters long"
+            }
           />
-        ) : (
-          <VisibilityIcon
-            sx={PasswordIconStyle}
-            onClick={setPasswordVisibilityTrue}
-          />
-        )}
+          {passwordVisibility ? (
+            <VisibilityOffIcon
+              sx={PasswordIconStyle}
+              onClick={setPasswordVisibilityFalse}
+            />
+          ) : (
+            <VisibilityIcon
+              sx={PasswordIconStyle}
+              onClick={setPasswordVisibilityTrue}
+            />
+          )}
+        </Box>
+        <Button variant="contained" color="error" size="small" type="submit">
+          {"SIGN IN"}
+        </Button>
+        <Button variant="text" color="error" type="reset">
+          {"RESET PASSWORD"}
+        </Button>
       </Box>
-      <Button variant="contained" color="error" size="small" type="submit">
-        {"SIGN IN"}
-      </Button>
-      <Button variant="text" color="error" type="reset">
-        {"RESET PASSWORD"}
-      </Button>
-    </FormControl>
+    </form>
   );
 };
