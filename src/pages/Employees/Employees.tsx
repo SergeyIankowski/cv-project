07ac6d13170262e46607325c5bb1ab7af7cc2942
SeregeyIankowski <garret933@gmail.com>
2023-05-17 08:20:49 +0300

    fix: fix searching om Emploees page

--- src/pages/Employees/Employees.tsx
+++ src/pages/Employees/Employees.tsx
@@ -1,13 +1,12 @@
 import {FC, useCallback, useEffect, useState} from "react";
 import HomeIcon from "@mui/icons-material/Home";
-import {PageLayout} from "@/components/view/PageLayout/PageLayout";
+import {PageLayout} from "@view/PageLayout/PageLayout";
+import {SearchInput} from "@view/SearchInput/SearchInput";
+import {TableEmployees} from "@containers/TableEmployees/TableEmployees";
 import {BreadCrumbsData} from "@/models/BreadCrumbsData.type";
 import {Pages} from "@/models/Pages";
-
-import {SearchInput} from "@/components/view/SearchInput/SearchInput";
-import {TableEmployees} from "@/components/containers/TableEmployees/TableEmployees";
-import {useEmployeesQuery} from "@/graphql/hooks/useEmployeesQuery";
 import {UserData} from "@/models/UserData.type";
+import {useEmployeesQuery} from "@/graphql/hooks/useEmployeesQuery";
 
 const breadCrumbs: BreadCrumbsData = [
   {
@@ -24,17 +23,20 @@ export const Employees: FC = () => {
   useEffect(() => {
     if (loading) return;
     setSearchedUsers(data);
-  });
+  }, [loading, data]);
 
-  const handleSearchingUsers = useCallback((value: string) => {
-    const newReference: UserData[] = [...data];
-    const usersOnMount = newReference.filter(item => {
-      const stringItem = Object.values(item).toString().toLocaleLowerCase();
-      const lowerCasedValue = value.toLowerCase();
-      return stringItem.includes(lowerCasedValue);
-    });
-    setSearchedUsers(usersOnMount);
-  }, []);
+  const handleSearchingUsers = useCallback(
+    (value: string) => {
+      const newReference: UserData[] = [...data];
+      const usersOnMount = newReference.filter(item => {
+        const stringItem = Object.values(item).toString().toLocaleLowerCase();
+        const lowerCasedValue = value.toLowerCase();
+        return stringItem.includes(lowerCasedValue);
+      });
+      setSearchedUsers(usersOnMount);
+    },
+    [data]
+  );
   return (
     <PageLayout linksData={breadCrumbs}>
       <SearchInput onSearch={handleSearchingUsers} />
