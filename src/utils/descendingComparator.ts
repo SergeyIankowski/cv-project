4c552ae046a11eaef9bae4959a0e1aa1d387735c
SeregeyIankowski <garret933@gmail.com>
2023-05-17 07:51:34 +0300

    fix: fix sorting function and initial order of items

--- src/utils/descendingComparator.ts
+++ src/utils/descendingComparator.ts
@@ -1,4 +1,17 @@
-export function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+import {Order} from "@/models/Order.type";
+
+export function descendingComparator<T>(
+  a: T,
+  b: T,
+  orderBy: keyof T,
+  order: Order
+) {
+  if (!a[orderBy]) {
+    return order === "desc" ? 1 : -1;
+  }
+  if (!b[orderBy]) {
+    return order === "desc" ? -1 : 1;
+  }
   if (b[orderBy] < a[orderBy]) {
     return -1;
   }
