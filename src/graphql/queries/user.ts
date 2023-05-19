61c8578a3541df1748f559eddb1eecff6a2e0401
SeregeyIankowski <garret933@gmail.com>
2023-05-19 12:32:56 +0300

    feat: create user query

---
+++ src/graphql/queries/user.ts
@@ -0,0 +1,20 @@
+import {gql} from "@apollo/client";
+
+export const USER = gql`
+  query user($id: ID!) {
+    user(id: $id) {
+      id
+      created_at
+      is_verified
+      profile {
+        avatar
+        first_name
+        last_name
+      }
+      email
+      department_name
+      position_name
+      role
+    }
+  }
+`;
