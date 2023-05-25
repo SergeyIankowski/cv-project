c30bec65f0c44dbe5bff87a5569ec3625d17d1c9
SeregeyIankowski <garret933@gmail.com>
2023-05-25 12:18:27 +0300

    feat: implement creating breadCrumbs when pages are changed

--- src/components/containers/ProfilePageLayout/ProfilePageLayout.tsx
+++ src/components/containers/ProfilePageLayout/ProfilePageLayout.tsx
@@ -1,10 +1,14 @@
-import {FC, ReactNode} from "react";
-import {useParams} from "react-router-dom";
+import {FC, ReactNode, useEffect, useState} from "react";
+import {useLocation, useParams} from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import {PageLayout} from "@view/PageLayout/PageLayout";
 import {Pages} from "@/models/Pages";
 import {BreadCrumbsData} from "@/models/BreadCrumbsData.type";
 import {ProfileTabs} from "@containers/ProfileTabs/ProfileTabs";
+import {useUserQuery} from "@/graphql/hooks/useUserQuery";
+import {BreadCrumb} from "@/models/BreadCrumb.type";
+import GroupIcon from "@mui/icons-material/Group";
+import {capitalize} from "@mui/material";
 
 interface ProfileLayoutProps {
   children: ReactNode;
@@ -21,8 +25,39 @@ const breadCrumbs: BreadCrumbsData = [
 
 export const ProfilePageLayout: FC<ProfileLayoutProps> = ({children}) => {
   const {id} = useParams();
+  const {pathname} = useLocation();
+
+  const {loadUserInfo, calledUserData, loadingUserData, userData} =
+    useUserQuery();
+  const [breadCrumbsData, setBreadCrumbsData] = useState(breadCrumbs);
+  useEffect(() => {
+    loadUserInfo(id!);
+  }, []);
+  useEffect(() => {
+    if (loadingUserData) return;
+    if (calledUserData && userData) {
+      const firstName = userData.user.profile.first_name;
+      const lastName = userData.user.profile.last_name;
+      const tabName = pathname.split("/").slice(-1).toString();
+
+      const newBreadCrumbName: BreadCrumb = {
+        text: `${firstName} ${lastName}`,
+        icon: <GroupIcon />,
+      };
+      const newBreadCrumbTab: BreadCrumb = {
+        text: capitalize(tabName),
+      };
+
+      const preparedBreadCrumbChain = [
+        ...breadCrumbs,
+        newBreadCrumbName,
+        newBreadCrumbTab,
+      ];
+      setBreadCrumbsData(preparedBreadCrumbChain);
+    }
+  }, [loadingUserData, userData, calledUserData, pathname]);
   return (
-    <PageLayout linksData={breadCrumbs}>
+    <PageLayout linksData={breadCrumbsData}>
       <ProfileTabs />
       {children}
     </PageLayout>
