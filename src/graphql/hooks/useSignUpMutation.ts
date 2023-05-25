3cae2d09c8e8e812110359856c27efcfa8e718a6
SeregeyIankowski <garret933@gmail.com>
2023-05-25 12:34:02 +0300

    feat: add AuthInfoService to useLoginQuery and useSignUpMutation custom graphql hooks

--- src/graphql/hooks/useSignUpMutation.ts
+++ src/graphql/hooks/useSignUpMutation.ts
@@ -2,6 +2,7 @@ import {useAuthToken} from "@/hooks/useAuthToken";
 import {useMutation} from "@apollo/client";
 import {SIGN_UP} from "../mutations";
 import {useEffect} from "react";
+import {AuthInfoService} from "@/services/AuthInfoService";
 
 export const useSignUpMutation = () => {
   const {setAuthToken} = useAuthToken();
@@ -9,7 +10,10 @@ export const useSignUpMutation = () => {
 
   useEffect(() => {
     if (error) return;
-    if (!loading && data) setAuthToken(data.signup.access_token);
+    if (!loading && data) {
+      setAuthToken(data.signup.access_token);
+      AuthInfoService.saveAuthInfo(data.login.user);
+    }
   }, [loading, error, data]);
 
   return {signUp};
