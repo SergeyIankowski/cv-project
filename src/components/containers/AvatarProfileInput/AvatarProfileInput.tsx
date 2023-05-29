ec98fa3a49389e47160d094ea696894d627cb431
SeregeyIankowski <garret933@gmail.com>
2023-05-29 10:19:07 +0300

    feat: implement uploading avatar

--- src/components/containers/AvatarProfileInput/AvatarProfileInput.tsx
+++ src/components/containers/AvatarProfileInput/AvatarProfileInput.tsx
@@ -1,4 +1,5 @@
-import {FC} from "react";
+import {ChangeEvent, FC} from "react";
+import {useParams} from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
@@ -7,14 +8,29 @@ import Grid from "@mui/material/Grid";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
 import {AvatarStyle, UploadHead} from "./AvatarProfileStyle";
 import Typography from "@mui/material/Typography";
+import {convertAvatarToRequestData} from "@/utils/convertAvatarToRequestData";
+import {useUploadAvatar} from "@/graphql/hooks/useUploadAvatar";
+import {AvatarData} from "@/models/AvatarData.type";
 
 interface AvatarProfileInputProps {
   avatarPath: string;
+  onLoadUserInfo: () => void;
 }
 
 export const AvatarProfileInput: FC<AvatarProfileInputProps> = ({
   avatarPath,
+  onLoadUserInfo,
 }) => {
+  const {uploadAvatar} = useUploadAvatar();
+  const {id} = useParams();
+  const uploadAvatarData = async (avatarData: AvatarData) => {
+    await uploadAvatar(id!, avatarData);
+    onLoadUserInfo();
+  };
+  const changeHandler = async (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file && id) convertAvatarToRequestData(file, uploadAvatarData);
+  };
   return (
     <Grid container justifyContent="center" gap="40px">
       <Box>
@@ -44,7 +60,11 @@ export const AvatarProfileInput: FC<AvatarProfileInputProps> = ({
             <Typography variant="subtitle1" sx={{color: "grey"}}>
               png, jpg or gif no more than 0.5MB
             </Typography>
-            <input type="file" style={{visibility: "hidden"}} />
+            <input
+              type="file"
+              style={{visibility: "hidden"}}
+              onChange={changeHandler}
+            />
           </Grid>
         </label>
       </Box>
