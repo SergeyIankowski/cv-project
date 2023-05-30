27fe7547156c9e81e552a806fa30c72a2e8c8045
SeregeyIankowski <garret933@gmail.com>
2023-05-30 12:18:26 +0300

    refactor: change variables names

--- src/hooks/useBreadCrumbs.tsx
+++ src/hooks/useBreadCrumbs.tsx
@@ -1,8 +1,9 @@
+import {useEffect, useState} from "react";
 import {useLocation} from "react-router-dom";
 import {BreadCrumbsData} from "@/models/BreadCrumbsData.type";
 import HomeIcon from "@mui/icons-material/Home";
+import GroupIcon from "@mui/icons-material/Group";
 import {Pages} from "@/models/Pages";
-import {useEffect, useState} from "react";
 import {useUserQuery} from "@/graphql/hooks/useUserQuery";
 import {createBreadCrumb} from "@/utils/createBreadCrumb";
 
@@ -18,34 +19,40 @@ export const useBreadCrumbs: () => BreadCrumbsData = () => {
   const {loadUserInfo, calledUserData, userData, loadingUserData} =
     useUserQuery();
   const {pathname} = useLocation();
-
   const [breadCrumbs, setBreadCrumbs] = useState<BreadCrumbsData>([]);
 
   useEffect(() => {
     const regExpForSeparating = /\/\w+/g;
     const names = pathname.match(regExpForSeparating);
     const crumbs = [...rootBreadCrumbs];
+
     if (names) {
       names.forEach((item, index, array) => {
-        const text = item.slice(1);
-        const isNumber = /^[0-9]+$/.test(text);
+        const textName = item.slice(1);
         const absolutePathForCrumb = array.slice(0, index + 1).join("");
+        const isNumber = /^[0-9]+$/.test(textName);
 
         if (isNumber && !calledUserData) {
-          loadUserInfo(Number(text));
+          loadUserInfo(Number(textName));
         }
 
         if (isNumber && calledUserData && !loadingUserData) {
           const firstName = userData.user.profile.first_name;
           const lastName = userData.user.profile.last_name;
           const initials = `${firstName} ${lastName}`;
-          const crumb = createBreadCrumb(initials, absolutePathForCrumb);
+          const crumb = createBreadCrumb(
+            initials,
+            absolutePathForCrumb,
+            <GroupIcon />
+          );
           crumbs.push(crumb);
           return;
         }
-        const crumb = createBreadCrumb(text, absolutePathForCrumb);
+
+        const crumb = createBreadCrumb(textName, absolutePathForCrumb);
         crumbs.push(crumb);
       });
+
       setBreadCrumbs(crumbs);
     }
   }, [calledUserData, loadingUserData, userData, pathname]);
