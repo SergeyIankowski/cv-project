8c6ada22303f98bdfc50209c5d0f2ec83baef6f6
SeregeyIankowski <garret933@gmail.com>
2023-05-31 14:38:59 +0300

    refactor: add useUserData hook and remove props transfer in ProfileInfo component

--- src/components/containers/ProfileInfo/ProfileInfo.tsx
+++ src/components/containers/ProfileInfo/ProfileInfo.tsx
@@ -1,21 +1,13 @@
-import {FC, useCallback, useEffect} from "react";
-import {useParams} from "react-router-dom";
+import {FC} from "react";
 import Grid from "@mui/material/Grid";
 import {AvatarProfileInput} from "@containers/AvatarProfileInput/AvatarProfileInput";
 import {ProfileUserInfo} from "@view/ProfileUserInfo/ProfileUserInfo";
-import {useUserQuery} from "@/graphql/hooks/useUserQuery";
 import {ProfileForm} from "@containers/ProfileForm/ProfileForm";
 import {ProgressSpinner} from "@/components/view/ProgressSpinner/ProgressSpinner";
+import {useUserData} from "@/hooks/useUserData";
 
 export const ProfileInfo: FC = () => {
-  const {id} = useParams();
-  const {loadUserInfo, called, userData} = useUserQuery();
-
-  const loadProfileInfo = useCallback(() => loadUserInfo(id!), [id]);
-
-  useEffect(() => {
-    if (id && !called) loadProfileInfo();
-  }, [id, userData, called]);
+  const {loadProfileInfo, called, userData} = useUserData();
 
   if (!called || !userData) return <ProgressSpinner />;
 
@@ -23,22 +15,11 @@ export const ProfileInfo: FC = () => {
     <Grid container direction="column" alignItems="center" sx={{p: "50px 0"}}>
       <Grid container direction="column" gap="30px" sx={{width: "700px"}}>
         <AvatarProfileInput
-          avatarPath={userData?.user.profile.avatar}
+          avatarPath={userData?.profile.avatar}
           onLoadUserInfo={loadProfileInfo}
         />
-        <ProfileUserInfo
-          firstName={userData?.user.profile?.first_name}
-          lastName={userData?.user.profile?.last_name}
-          email={userData?.user.email}
-          createdAt={userData?.user.created_at}
-        />
-        <ProfileForm
-          firstName={userData?.user.profile?.first_name}
-          lastName={userData?.user.profile?.last_name}
-          department={userData?.user.department_name}
-          position={userData?.user.position_name}
-          onLoadUserInfo={loadProfileInfo}
-        />
+        <ProfileUserInfo />
+        <ProfileForm />
       </Grid>
     </Grid>
   );
