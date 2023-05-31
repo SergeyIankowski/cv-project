8571bd2f29912d5fc5c856deae8f2a11d43d8fb2
SeregeyIankowski <garret933@gmail.com>
2023-05-31 14:32:36 +0300

    feat: add useUserData hook to MainHeader for user avatar and name displaying

--- src/components/view/MainHeader/MainHeader.tsx
+++ src/components/view/MainHeader/MainHeader.tsx
@@ -1,21 +1,25 @@
+import {FC} from "react";
 import Box from "@mui/material/Box";
 import MenuIcon from "@mui/icons-material/Menu";
-import {UserDetails} from "@containers/UserDetails/UserDetails";
-import {MainHeaderStyle} from "@view/MuiPagesStyles";
-import {SlideMenu} from "@containers/SlideMenu/SlideMenu";
-import {FC} from "react";
-
 import {BoxProps} from "@mui/material/Box";
+import {MainHeaderStyle} from "@view/MuiPagesStyles";
+import {UserDetails} from "@containers/UserDetails/UserDetails";
+import {SlideMenu} from "@containers/SlideMenu/SlideMenu";
 import {useBooleanState} from "@/hooks/useBooleanState";
+import {useUserData} from "@/hooks/useUserData";
 
 type MainHeaderProps = BoxProps;
 
 export const MainHeader: FC<MainHeaderProps> = () => {
   const [isOpen, setTrue, setFalse] = useBooleanState(false);
+  const {userData} = useUserData();
   return (
     <Box component={"header"} sx={MainHeaderStyle}>
       <MenuIcon onClick={setTrue} />
-      <UserDetails userName="Sergei" />
+      <UserDetails
+        userName={`${userData?.profile?.first_name} ${userData?.profile?.last_name}`}
+        avatar={userData?.profile?.avatar}
+      />
       <SlideMenu isOpen={isOpen} onCloseVisibility={setFalse} />
     </Box>
   );
