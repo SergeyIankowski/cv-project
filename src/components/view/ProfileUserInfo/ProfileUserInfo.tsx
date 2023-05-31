88a8bbd0d047262232c46765779b905e3e2fd535
SeregeyIankowski <garret933@gmail.com>
2023-05-31 14:30:37 +0300

    feat: remove props and add useUserData hook to ProfileUserInfo

--- src/components/view/ProfileUserInfo/ProfileUserInfo.tsx
+++ src/components/view/ProfileUserInfo/ProfileUserInfo.tsx
@@ -1,13 +1,7 @@
 import {FC, useEffect, useState} from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-
-interface ProfileUserInfoProps {
-  firstName: string;
-  lastName: string;
-  email: string;
-  createdAt: string;
-}
+import {useUserData} from "@/hooks/useUserData";
 
 const DATE_OPTIONS: Intl.DateTimeFormatOptions = {
   weekday: "long",
@@ -16,25 +10,21 @@ const DATE_OPTIONS: Intl.DateTimeFormatOptions = {
   day: "numeric",
 };
 
-export const ProfileUserInfo: FC<ProfileUserInfoProps> = ({
-  firstName,
-  lastName,
-  email,
-  createdAt,
-}) => {
+export const ProfileUserInfo: FC = () => {
   const [date, setDate] = useState<Date>(new Date());
+  const {userData} = useUserData();
 
   useEffect(() => {
-    const mountedDate = new Date(Number(createdAt));
+    const mountedDate = new Date(Number(userData.created_at));
     setDate(mountedDate);
-  }, [createdAt]);
+  }, [userData.created_at]);
 
   return (
     <Box>
       <Typography variant="h4">
-        {firstName} {lastName}
+        {userData.profile.first_name} {userData.profile.last_name}
       </Typography>
-      <Typography variant="subtitle1">{email}</Typography>
+      <Typography variant="subtitle1">{userData.email}</Typography>
       <Typography variant="subtitle1">
         A member since {date.toLocaleDateString("en-US", DATE_OPTIONS)}
       </Typography>
