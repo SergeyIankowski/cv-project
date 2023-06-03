3617f7e62bea7cc6173db75157fc2d8fb59c3a16
SeregeyIankowski <garret933@gmail.com>
2023-06-03 12:50:48 +0300

    feat: create TableRowProjects component

---
+++ src/components/view/TableRowProjects/TableRowProjects.tsx
@@ -0,0 +1,22 @@
+import {FC} from "react";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import {ProjectData} from "@/models/ProjectData.type";
+
+interface TableRowProjectsProps {
+  row: ProjectData;
+}
+
+export const TableRowProjects: FC<TableRowProjectsProps> = ({row}) => {
+  return (
+    <TableRow>
+      <TableCell align="right">{row.name}</TableCell>
+      <TableCell align="right">{row.internalName}</TableCell>
+      <TableCell align="right">{row.domain}</TableCell>
+      <TableCell align="right">{row.startDate}</TableCell>
+      <TableCell align="right">{row.endDate}</TableCell>
+      <TableCell align="right">{row.teamSize}</TableCell>
+      <TableCell align="right"></TableCell>
+    </TableRow>
+  );
+};
