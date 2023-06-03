6d285ec2c3ebb01c763aa709b59cee56dcacc4f1
SeregeyIankowski <garret933@gmail.com>
2023-06-03 13:36:27 +0300

    feat: add useProjectsQuery hook to Projects page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,6 +1,9 @@
 import {FC} from "react";
 import {TableProjects} from "@/components/containers/TableProjects/TableProjects";
+import {useProjectsQuery} from "@/graphql/hooks/useProjectsQuery";
 
 export const Projects: FC = () => {
-  return <TableProjects />;
+  const {data} = useProjectsQuery();
+
+  return <TableProjects projectsData={data} />;
 };
