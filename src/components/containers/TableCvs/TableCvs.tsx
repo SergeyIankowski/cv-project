0b79e8c4a8f5893b4ab8949df78e9130d3b74b3d
SeregeyIankowski <garret933@gmail.com>
2023-06-04 12:39:47 +0300

    feat: create TableCvs component

---
+++ src/components/containers/TableCvs/TableCvs.tsx
@@ -0,0 +1,40 @@
+import {FC} from "react";
+import TableBody from "@mui/material/TableBody";
+import {TableTemplate} from "@view/TableTemplate/TableTemplate";
+import {useSort} from "@/hooks/useSort";
+import {CvData} from "@/models/CvData";
+import {Order} from "@/models/Order.type";
+import {TableHeadTemplate} from "@view/TableHeadTemplate/TableHeadTemplate";
+import {TableRowCvs} from "@/components/view/TableRowCvs/TableRowCvs";
+import {cvsHeadCellsData} from "./cvsHeadCellsData";
+
+interface TableCvsProps {
+  cvsData: CvData[];
+}
+
+const DEFAULT_ORDER: Order = "asc";
+const DEFAULT_ORDER_BY: keyof CvData = "name";
+
+export const TableCvs: FC<TableCvsProps> = ({cvsData}) => {
+  const [order, orderBy, sortedRowsOnPage, handleRequestSort] = useSort<CvData>(
+    DEFAULT_ORDER,
+    DEFAULT_ORDER_BY,
+    cvsData
+  );
+  return (
+    <TableTemplate>
+      <TableHeadTemplate<CvData>
+        headCells={cvsHeadCellsData}
+        order={order}
+        orderBy={orderBy}
+        hasControlsColumn
+        onRequestSort={handleRequestSort}
+      />
+      <TableBody>
+        {sortedRowsOnPage.map(cv => (
+          <TableRowCvs key={cv.name} row={cv} />
+        ))}
+      </TableBody>
+    </TableTemplate>
+  );
+};
