7b424578286ce547d107c44597b02ba39d58a198
SeregeyIankowski <garret933@gmail.com>
2023-06-05 12:31:11 +0300

    feat: create TableRowLanguages

---
+++ src/components/view/TableRowLanguages/TableRowLanguages.tsx
@@ -0,0 +1,24 @@
+import {FC} from "react";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import {LanguagesData} from "@/models/LanguagesData";
+import {LanguagesRowControlMenu} from "@view/LanguagesRowControlMenu/LanguagesRowControlMenu";
+
+interface TableRowLanguagesProps {
+  row: LanguagesData;
+}
+
+export const TableRowLanguages: FC<TableRowLanguagesProps> = ({row}) => {
+  return (
+    <TableRow>
+      <TableCell></TableCell>
+      <TableCell align="right">{row.name}</TableCell>
+      <TableCell align="right">{row.nativeName}</TableCell>
+      <TableCell align="right">{row.iso2}</TableCell>
+
+      <TableCell align="right">
+        <LanguagesRowControlMenu id={row.id} />
+      </TableCell>
+    </TableRow>
+  );
+};
