57e713d281d5b02960e662d046c88fe698488b9a
SeregeyIankowski <garret933@gmail.com>
2023-06-05 14:54:10 +0300

    feat: add click handler for changing page when slide menu item is clicked

--- src/components/view/ListLinks/ListLinks.tsx
+++ src/components/view/ListLinks/ListLinks.tsx
@@ -1,8 +1,9 @@
+import {FC} from "react";
+import {useNavigate} from "react-router-dom";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemButton from "@mui/material/ListItemButton";
-import {FC} from "react";
 import {LinksData} from "@/models/LinksData.type";
 
 interface ListLinksProps {
@@ -10,11 +11,16 @@ interface ListLinksProps {
 }
 
 export const ListLinks: FC<ListLinksProps> = ({linksData}) => {
+  const navigate = useNavigate();
   return (
     <>
       {linksData.map(obj => (
         <ListItem key={obj.linkName} disablePadding>
-          <ListItemButton>
+          <ListItemButton
+            onClick={() => {
+              obj.pathToRouting ? navigate(obj.pathToRouting) : "";
+            }}
+          >
             {obj.iconNode && <ListItemIcon>{obj.iconNode}</ListItemIcon>}
             <ListItemText primary={obj.linkName} />
           </ListItemButton>
