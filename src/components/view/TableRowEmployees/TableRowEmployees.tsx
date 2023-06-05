600152959c22cb97178007f707a58f308c8ad634
SergeyIankowski <92864057+SergeyIankowski@users.noreply.github.com>
2023-06-05 11:09:36 +0300

    Fix/sy/app styles (#35)
    * refactor: change widths in app
    
    * refactor: change avatar image tag from img to Avatar

--- src/components/view/TableRowEmployees/TableRowEmployees.tsx
+++ src/components/view/TableRowEmployees/TableRowEmployees.tsx
@@ -1,9 +1,9 @@
 import {FC} from "react";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
+import {Avatar} from "@mui/material";
 import DefaultUser from "@/assets/defaultUser.png";
 import {UserData} from "@/models/UserData.type";
-
 import {EmployeesRowControlMenu} from "../EmployeesRowControlMenu/EmployeesRowControlMenu";
 
 interface TableRowTemplateProps {
@@ -13,11 +13,8 @@ interface TableRowTemplateProps {
 export const TableRowEmployees: FC<TableRowTemplateProps> = ({row}) => {
   return (
     <TableRow>
-      <TableCell align="center">
-        <img
-          src={row.imgPath ? row.imgPath : DefaultUser}
-          style={{borderRadius: "50%", width: "30px", height: "30px"}}
-        />
+      <TableCell>
+        <Avatar src={row.imgPath ? row.imgPath : DefaultUser} />
       </TableCell>
       <TableCell align="right">{row.firstName}</TableCell>
       <TableCell align="right">{row.lastName}</TableCell>
