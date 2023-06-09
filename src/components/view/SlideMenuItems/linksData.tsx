a9007847b73ae2b58912fe23e19e347fee82f43f
SergeyIankowski <92864057+SergeyIankowski@users.noreply.github.com>
2023-06-09 12:02:02 +0300

    Feat/sy/slide menu switching pages (#41)
    * feat: add PathToRouting prop to linksData type
    
    * feat: add paths to LinksData
    
    * feat: add click handler for changing page when slide menu item is clicked
    
    * refactor: delete path to languages in slide menu

--- src/components/view/SlideMenuItems/linksData.tsx
+++ src/components/view/SlideMenuItems/linksData.tsx
@@ -2,13 +2,38 @@ import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
 import GTranslateIcon from "@mui/icons-material/GTranslate";
 import HomeRepairServiceIcon from "@mui/icons-material/HomeRepairService";
 import {LinksData} from "@/models/LinksData.type";
+import {Pages} from "@/models/Pages";
 
 export const linksData: LinksData[] = [
-  {linkName: "Employees", iconNode: <SupervisorAccountIcon />},
-  {linkName: "Projects", iconNode: <HomeRepairServiceIcon />},
-  {linkName: "CV's", iconNode: null},
-  {linkName: "Departments", iconNode: null},
-  {linkName: "Positions", iconNode: null},
-  {linkName: "Skills", iconNode: null},
-  {linkName: "Languages", iconNode: <GTranslateIcon />},
+  {
+    linkName: "Employees",
+    iconNode: <SupervisorAccountIcon />,
+    pathToRouting: `${Pages.main.root}${Pages.main.employees}`,
+  },
+  {
+    linkName: "Projects",
+    iconNode: <HomeRepairServiceIcon />,
+    pathToRouting: `/${Pages.main.projects}`,
+  },
+  {
+    linkName: "CV's",
+    iconNode: null,
+    pathToRouting: `${Pages.main.root}${Pages.main.cvs}`,
+  },
+  {
+    linkName: "Departments",
+    iconNode: null,
+  },
+  {
+    linkName: "Positions",
+    iconNode: null,
+  },
+  {
+    linkName: "Skills",
+    iconNode: null,
+  },
+  {
+    linkName: "Languages",
+    iconNode: <GTranslateIcon />,
+  },
 ];
--- src/models/LinksData.type.ts
+++ src/models/LinksData.type.ts
@@ -3,4 +3,5 @@ import {ReactNode} from "react";
 export interface LinksData {
   linkName: string;
   iconNode: ReactNode | null;
+  pathToRouting?: string;
 }
