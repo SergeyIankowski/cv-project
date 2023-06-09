e3f5c83d9dddcbd4a4adf2df5c79b9ec1f3138ca
SergeyIankowski <92864057+SergeyIankowski@users.noreply.github.com>
2023-06-09 12:01:06 +0300

    Feat/sy/use table searh hook (#39)
    * feat: create useTableSearch custom hook
    
    * refactor: add useSearchHook to Employees Page
    
    * refactor: refactor displaying control item when there is no icon

--- src/pages/Employees/Employees.tsx
+++ src/pages/Employees/Employees.tsx
@@ -1,33 +1,21 @@
-import {FC, useCallback, useEffect, useState} from "react";
+import {FC} from "react";
 import {SearchInput} from "@view/SearchInput/SearchInput";
 import {TableEmployees} from "@containers/TableEmployees/TableEmployees";
 import {UserData} from "@/models/UserData.type";
 import {useEmployeesQuery} from "@/graphql/hooks/useEmployeesQuery";
+import {useTableSearch} from "@/hooks/useTableSearch";
 
 export const Employees: FC = () => {
   const {loading, data} = useEmployeesQuery();
-  const [searchedUsers, setSearchedUsers] = useState<UserData[]>([]);
-  useEffect(() => {
-    if (loading) return;
-    setSearchedUsers(data);
-  }, [loading, data]);
-
-  const handleSearchingUsers = useCallback(
-    (value: string) => {
-      const newReference: UserData[] = [...data];
-      const usersOnMount = newReference.filter(item => {
-        const stringItem = Object.values(item).toString().toLocaleLowerCase();
-        const lowerCasedValue = value.toLowerCase();
-        return stringItem.includes(lowerCasedValue);
-      });
-      setSearchedUsers(usersOnMount);
-    },
-    [data]
+  const {searchedData, handleSearchingData} = useTableSearch<UserData>(
+    data,
+    loading
   );
+
   return (
     <>
-      <SearchInput onSearch={handleSearchingUsers} />
-      <TableEmployees usersData={searchedUsers} />
+      <SearchInput onSearch={handleSearchingData} />
+      <TableEmployees usersData={searchedData} />
     </>
   );
 };
