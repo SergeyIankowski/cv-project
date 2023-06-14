ba58fdbd244b9343eb66e7ca93d98b5a6fa1ec3c
SeregeyIankowski <garret933@gmail.com>
2023-06-14 14:36:00 +0300

    feat: create CREATE_USER mutation

--- src/graphql/mutations/index.ts
+++ src/graphql/mutations/index.ts
@@ -2,5 +2,6 @@ import {SIGN_UP} from "./signup";
 import {UPDATE_USER} from "./updateUser";
 import {UPLOAD_AVATAR} from "./uploadAvatar";
 import {DELETE_AVATAR} from "./deleteAvatar";
+import {CREATE_USER} from "./createUser";
 
-export {SIGN_UP, UPLOAD_AVATAR, DELETE_AVATAR, UPDATE_USER};
+export {SIGN_UP, UPLOAD_AVATAR, DELETE_AVATAR, UPDATE_USER, CREATE_USER};
