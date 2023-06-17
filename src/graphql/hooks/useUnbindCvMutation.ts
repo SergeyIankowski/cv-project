b47d84be854e59a4c35fa8d19e03aaad7233ff7e
SeregeyIankowski <garret933@gmail.com>
2023-06-17 16:10:37 +0300

    feat: add refetchQueries to useUnbindCvMutation

--- src/graphql/hooks/useUnbindCvMutation.ts
+++ src/graphql/hooks/useUnbindCvMutation.ts
@@ -1,9 +1,10 @@
-import {useMutation} from "@apollo/client";
-import {UNBIND_CV} from "../mutations";
 import {useCallback} from "react";
+import {useMutation} from "@apollo/client";
+import {UNBIND_CV} from "@/graphql/mutations";
+import {USER} from "@/graphql/queries";
 
 export const useUnbindCvMutation = () => {
-  const [loadData] = useMutation(UNBIND_CV);
+  const [loadData] = useMutation(UNBIND_CV, {refetchQueries: [USER]});
 
   const unbindCv = useCallback(
     (id: string | number) => loadData({variables: {id}}),
