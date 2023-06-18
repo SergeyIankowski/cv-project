cc17d4f89d8a66fb1e81047a562b775c578d4d61
SeregeyIankowski <garret933@gmail.com>
2023-06-18 13:10:41 +0300

    feat: create AccordionTemplate component

---
+++ src/components/view/AccordionTemplate/index.tsx
@@ -0,0 +1,36 @@
+import {FC, ReactNode} from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Typography from "@mui/material/Typography";
+import {colors} from "@view/MuiPagesStyles";
+
+interface AccordionTemplateProps {
+  title: string;
+  children: ReactNode;
+}
+
+export const AccordionTemplate: FC<AccordionTemplateProps> = ({
+  title,
+  children,
+}) => {
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon style={{color: colors.authLinksColor}} />}
+        aria-controls="panel1a-content"
+        id={String(title)}
+        sx={{backgroundColor: colors.headerBgColor}}
+      >
+        <Typography
+          variant="h5"
+          sx={{color: colors.authLinksColor, borderColor: "white"}}
+        >
+          {title}
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>{children}</AccordionDetails>
+    </Accordion>
+  );
+};
