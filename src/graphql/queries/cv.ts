22c993e361dd87b7572508d83298c506e0ee082c
SeregeyIankowski <garret933@gmail.com>
2023-06-20 11:13:42 +0300

    feat: add new response fields to CV query

--- src/graphql/queries/cv.ts
+++ src/graphql/queries/cv.ts
@@ -5,6 +5,15 @@ export const CV = gql`
     cv(id: $id) {
       name
       description
+
+      skills {
+        skill_name
+        mastery
+      }
+      languages {
+        language_name
+        proficiency
+      }
       is_template
     }
   }
