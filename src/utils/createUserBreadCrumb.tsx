b09d18c213643c4be69b218a2fce808a8029f2ca
SeregeyIankowski <garret933@gmail.com>
2023-06-22 08:29:06 +0300

    refactor: remove triggering callback from createUserBreadcrumb function

--- src/utils/createUserBreadCrumb.tsx
+++ src/utils/createUserBreadCrumb.tsx
@@ -1,31 +1,14 @@
 import GroupIcon from "@mui/icons-material/Group";
-import {BreadCrumb} from "@/models/BreadCrumb.type";
 import {FetchedUser} from "@/models/FetchedUser.type";
 import {createBreadCrumb} from "./createBreadCrumb";
 
 export const createUserBreadCrumb = (
-  isNumber: boolean,
-  called: boolean,
-  loading: boolean,
   userData: FetchedUser,
-  crumbs: BreadCrumb[],
-  id: number,
-  absolutePathForCrumb: string,
-  loadDataCallback: (id: number) => void
+  absolutePathForCrumb: string
 ) => {
-  if (isNumber && !called) {
-    loadDataCallback(id);
-  }
-
-  if (isNumber && called && !loading && userData) {
-    const fullName = userData.profile.full_name;
-    const email = userData.email;
-    const initials = fullName ? fullName : email;
-    const crumb = createBreadCrumb(
-      initials,
-      absolutePathForCrumb,
-      <GroupIcon />
-    );
-    crumbs.push(crumb);
-  }
+  const fullName = userData.profile.full_name;
+  const email = userData.email;
+  const initials = fullName ? fullName : email;
+  const crumb = createBreadCrumb(initials, absolutePathForCrumb, <GroupIcon />);
+  return crumb;
 };
