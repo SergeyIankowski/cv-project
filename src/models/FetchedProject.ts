44b4fc5e51d1ff380adf9b5859fa50731fe6ea7b
SeregeyIankowski <garret933@gmail.com>
2023-06-22 07:31:34 +0300

    feat: create FetchedProject type

---
+++ src/models/FetchedProject.ts
@@ -0,0 +1,10 @@
+export interface FetchedProject {
+  id: string;
+  name: string;
+  internal_name: string;
+  description: string;
+  domain: string;
+  start_date: string;
+  end_date: string;
+  team_size: string;
+}
