35164ca670c7e3ab5bac7798c96f0f5d1b30548c
SeregeyIankowski <garret933@gmail.com>
2023-07-01 07:04:24 +0300

    feat: add refetchQueries to useCreateCv

--- src/graphql/hooks/useCreateCv.ts
+++ src/graphql/hooks/useCreateCv.ts
@@ -1,13 +1,15 @@
 import {useMutation} from "@apollo/client";
-import {CREATE_CV} from "../mutations";
-import {CvInput} from "../interfaces/CvInput.interface";
+import {CREATE_CV} from "@/graphql/mutations";
+import {CvInput} from "@/graphql/interfaces/CvInput.interface";
+import {CV} from "@/graphql/queries";
 
 export const useCreateCv = () => {
-  const [loadData] = useMutation(CREATE_CV);
-
+  const [loadData, {called, loading, data, error}] = useMutation(CREATE_CV, {
+    refetchQueries: [CV],
+  });
   const createCv = (cv: CvInput) => {
     loadData({variables: {cv}});
   };
 
-  return {createCv};
+  return {createCv, called, data, loading, error};
 };
