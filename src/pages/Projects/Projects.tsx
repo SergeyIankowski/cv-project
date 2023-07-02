8d577b3c6289afe70082d366123f0aaa0d093038
SeregeyIankowski <garret933@gmail.com>
2023-07-02 08:17:00 +0300

    feat: add search input to Projects page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,9 +1,24 @@
 import {FC} from "react";
 import {TableProjects} from "@/pages/Projects/TableProjects/TableProjects";
 import {useProjectsQuery} from "@/graphql/hooks/useProjectsQuery";
+import {PageLayoutRowContainer} from "@/components/view/PageLayoutRowContainer";
+import {SearchInput} from "@view/SearchInput/SearchInput";
+import {useTableSearch} from "@/hooks/useTableSearch";
+import {ProjectTableData} from "@/models/TableDataTypes";
 
 export const Projects: FC = () => {
-  const {tableProjects} = useProjectsQuery();
+  const {tableProjects, loading} = useProjectsQuery();
+  const {searchedData, handleSearchingData} = useTableSearch<ProjectTableData>(
+    tableProjects,
+    loading
+  );
 
-  return <TableProjects projectsData={tableProjects} />;
+  return (
+    <>
+      <PageLayoutRowContainer>
+        <SearchInput onSearch={handleSearchingData} />
+      </PageLayoutRowContainer>
+      <TableProjects projectsData={searchedData} />;
+    </>
+  );
 };
