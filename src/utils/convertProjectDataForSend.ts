5221dc1bd2ead8274ef9bd2153f4ed6900e17d82
SeregeyIankowski <garret933@gmail.com>
2023-07-02 10:24:26 +0300

    refactor: change fields type in convertProjectDataForSend

--- src/utils/convertProjectDataForSend.ts
+++ src/utils/convertProjectDataForSend.ts
@@ -1,9 +1,9 @@
-import {UpdateProjectFormFields} from "@/models/FormFieldsTypes/UpdateProjectFormFields.type";
+import {ProjectFormFields} from "@/models/FormFieldsTypes";
 import {PROJECT_FORM_KEYS} from "@/models/FormKeysNames";
 
 export const convertProjectDataForSend = (
-  data: UpdateProjectFormFields
-): Required<UpdateProjectFormFields> => {
+  data: ProjectFormFields
+): Required<ProjectFormFields> => {
   return {
     [PROJECT_FORM_KEYS.name]: data.name,
     [PROJECT_FORM_KEYS.internal_name]: data.internal_name,
@@ -12,6 +12,6 @@ export const convertProjectDataForSend = (
     [PROJECT_FORM_KEYS.start_date]: data.start_date,
     [PROJECT_FORM_KEYS.end_date]: data.end_date,
     [PROJECT_FORM_KEYS.team_size]: Number(data.team_size),
-    [PROJECT_FORM_KEYS.skillsIds]: [],
+    [PROJECT_FORM_KEYS.skillsIds]: data.skillsIds ? [...data.skillsIds] : [],
   };
 };
