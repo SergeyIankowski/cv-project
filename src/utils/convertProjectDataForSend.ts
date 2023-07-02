7308bf1e36cb312f94bdff564d807742dfaf0fc0
SeregeyIankowski <garret933@gmail.com>
2023-07-02 10:52:48 +0300

    refactor: refactor returned fields in convertProjectDataForSend

--- src/utils/convertProjectDataForSend.ts
+++ src/utils/convertProjectDataForSend.ts
@@ -6,11 +6,11 @@ export const convertProjectDataForSend = (
 ): Required<ProjectFormFields> => {
   return {
     [PROJECT_FORM_KEYS.name]: data.name,
-    [PROJECT_FORM_KEYS.internal_name]: data.internal_name,
+    [PROJECT_FORM_KEYS.internal_name]: data.internal_name || "",
     [PROJECT_FORM_KEYS.description]: data.description,
     [PROJECT_FORM_KEYS.domain]: data.domain,
     [PROJECT_FORM_KEYS.start_date]: data.start_date,
-    [PROJECT_FORM_KEYS.end_date]: data.end_date,
+    [PROJECT_FORM_KEYS.end_date]: data.end_date || "",
     [PROJECT_FORM_KEYS.team_size]: Number(data.team_size),
     [PROJECT_FORM_KEYS.skillsIds]: data.skillsIds ? [...data.skillsIds] : [],
   };
