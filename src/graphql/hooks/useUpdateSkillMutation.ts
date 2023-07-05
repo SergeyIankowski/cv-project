974c184b90f41be08fd10c49e4d6a810c23cf3ae
SeregeyIankowski <garret933@gmail.com>
2023-07-05 11:57:53 +0300

    feat: add to useUpdateSkillMutation refetching SKILLS query

--- src/graphql/hooks/useUpdateSkillMutation.ts
+++ src/graphql/hooks/useUpdateSkillMutation.ts
@@ -2,9 +2,10 @@ import {useMutation} from "@apollo/client";
 import {UPDATE_SKILL} from "../mutations";
 import {Skill} from "../interfaces/Skill.interface";
 import {SkillInput} from "../interfaces/SkillInput";
+import {SKILLS} from "../queries";
 
 export const useUpdateSkillMutation = () => {
-  const [loadData] = useMutation(UPDATE_SKILL);
+  const [loadData] = useMutation(UPDATE_SKILL, {refetchQueries: [SKILLS]});
   const updateSkill = async (id: Skill["id"], skill: SkillInput) => {
     await loadData({variables: {id, skill}});
   };
