c4aca37e057c99f2c838cdc9e67a9b58d9a7f2c3
SeregeyIankowski <garret933@gmail.com>
2023-07-05 08:27:32 +0300

    feat: create useDeleteDepartment hook

---
+++ src/graphql/hooks/useDeleteDepartment.ts
@@ -0,0 +1,15 @@
+import {useMutation} from "@apollo/client";
+import {DELETE_DEPARTMENT} from "@/graphql/mutations";
+import {DEPARTMENTS} from "@/graphql/queries";
+import {Department} from "../interfaces/Department.interface";
+
+export const useDeleteDepartment = () => {
+  const [loadData] = useMutation(DELETE_DEPARTMENT, {
+    refetchQueries: [DEPARTMENTS],
+  });
+  const deleteDepartment = async (id: Department["id"]) => {
+    await loadData({variables: {id}});
+  };
+
+  return {deleteDepartment};
+};
