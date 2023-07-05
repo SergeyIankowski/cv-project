723155e7ea836cd52ac80cc892cc248c067e27dc
SeregeyIankowski <garret933@gmail.com>
2023-07-05 18:05:13 +0300

    feat: add fetchPolicy to usePositionsQuery

--- src/graphql/hooks/usePositionsQuery.ts
+++ src/graphql/hooks/usePositionsQuery.ts
@@ -1,10 +1,13 @@
 import {useQuery} from "@apollo/client";
 import {POSITIONS} from "../queries";
 import {useEffect, useState} from "react";
+import {FETCH_POLICY} from "../fetchPolicy";
 
 export const usePositionsQuery = () => {
   const [positions, setPositions] = useState([]);
-  const {data, loading, error} = useQuery(POSITIONS);
+  const {data, loading, error} = useQuery(POSITIONS, {
+    fetchPolicy: FETCH_POLICY.cacheAndNetwork,
+  });
 
   useEffect(() => {
     if (loading) return;
