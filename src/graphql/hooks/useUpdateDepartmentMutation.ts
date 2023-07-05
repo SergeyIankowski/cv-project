fe6d25bd353149d02991632f49196cbf359f836e
SeregeyIankowski <garret933@gmail.com>
2023-07-05 09:33:17 +0300

    feat: add refetching query DEPARTMENTS to useUpdateDepartments

--- src/graphql/hooks/useUpdateDepartmentMutation.ts
+++ src/graphql/hooks/useUpdateDepartmentMutation.ts
@@ -2,9 +2,12 @@ import {useMutation} from "@apollo/client";
 import {UPDATE_DEPARTMENT} from "@/graphql/mutations";
 import {Department} from "@/graphql/interfaces/Department.interface";
 import {DepartmentInput} from "@/graphql/interfaces/DepartmentInput.type";
+import {DEPARTMENTS} from "../queries";
 
 export const useUpdateDepartmentMutation = () => {
-  const [loadData] = useMutation(UPDATE_DEPARTMENT);
+  const [loadData] = useMutation(UPDATE_DEPARTMENT, {
+    refetchQueries: [DEPARTMENTS],
+  });
   const updateDepartment = async (
     id: Department["id"],
     department: DepartmentInput
