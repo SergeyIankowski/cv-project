4a981901f8e8bb0f55252b1b1702b5fd47ba21ce
SeregeyIankowski <garret933@gmail.com>
2023-07-07 13:24:01 +0300

    feat: create UpdateLanguageForm

---
+++ src/pages/Languages/UpdateLanguageForm/index.tsx
@@ -0,0 +1,62 @@
+import {FC, useContext} from "react";
+import {useForm} from "react-hook-form";
+import Box from "@mui/material/Box";
+import {Input} from "@containers/Input";
+import {Button} from "@containers/Button";
+import {ModalTemplateContext} from "@view/ModalTemplate/ModalTemplateContext";
+import {ModalLayout} from "@view/MuiPagesStyles";
+import {Language} from "@/graphql/interfaces/Language.interface";
+import {LanguageInput} from "@/graphql/interfaces/LanguageInput.type";
+import {UpdateLanguageFormFields} from "@/models/FormFieldsTypes";
+import {useUpdateLanguageMutation} from "@/graphql/hooks/useUpdateLanguageMutation";
+
+interface UpdateLanguageFormProps {
+  id: Language["id"];
+  language: LanguageInput;
+}
+
+export const UpdateLanguageForm: FC<UpdateLanguageFormProps> = ({
+  id,
+  language,
+}) => {
+  const {control, handleSubmit} = useForm<UpdateLanguageFormFields>({
+    defaultValues: {
+      name: language.name,
+      native_name: language.native_name,
+      iso2: language.iso2,
+    },
+  });
+  const {closeModal} = useContext(ModalTemplateContext);
+  const {updateLanguage} = useUpdateLanguageMutation();
+  const onSubmit = async (data: LanguageInput) => {
+    await updateLanguage(id, data);
+    closeModal();
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box sx={ModalLayout}>
+        <Input<UpdateLanguageFormFields>
+          control={control}
+          id="name"
+          label="Name"
+          name="name"
+        />
+        <Input<UpdateLanguageFormFields>
+          control={control}
+          id="native_name"
+          label="Native Name"
+          name="native_name"
+        />
+        <Input<UpdateLanguageFormFields>
+          control={control}
+          id="iso2"
+          label="ISO2"
+          name="iso2"
+        />
+        <Button variant="contained" color="error" size="small" type="submit">
+          Update
+        </Button>
+      </Box>
+    </form>
+  );
+};
