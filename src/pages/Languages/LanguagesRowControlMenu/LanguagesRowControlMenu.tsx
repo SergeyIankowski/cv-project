225e41911196e1396ab743e5184aaa9271086ffe
SeregeyIankowski <garret933@gmail.com>
2023-07-07 13:25:56 +0300

    feat: add UpdatelanguageModal to Languages page

--- src/pages/Languages/LanguagesRowControlMenu/LanguagesRowControlMenu.tsx
+++ src/pages/Languages/LanguagesRowControlMenu/LanguagesRowControlMenu.tsx
@@ -1,4 +1,4 @@
-import {FC, useCallback} from "react";
+import {FC, useCallback, useContext} from "react";
 import {
   RowControlMenuTemplate,
   TableRowControls,
@@ -6,6 +6,7 @@ import {
 import {LanguagesTableData} from "@/models/TableDataTypes";
 import {useDeleteLanguageMutation} from "@/graphql/hooks/useDeleteLanguageMutation";
 import {AuthInfoService} from "@/services/AuthInfoService";
+import {ModalTemplateContext} from "@/components/view/ModalTemplate/ModalTemplateContext";
 
 interface LanguagesRowControlMenuProps {
   id: LanguagesTableData["id"];
@@ -15,11 +16,14 @@ export const LanguagesRowControlMenu: FC<LanguagesRowControlMenuProps> = ({
   id,
 }) => {
   const {deleteLanguage} = useDeleteLanguageMutation();
+  const {openModal: openUpdateModal} = useContext(ModalTemplateContext);
   const data: TableRowControls = [
     {
       text: "Update",
       icon: "",
-      clickCallback: useCallback(() => {}, []),
+      clickCallback: useCallback(() => {
+        openUpdateModal();
+      }, []),
       disabled: AuthInfoService.isNotAdmin(),
     },
     {
