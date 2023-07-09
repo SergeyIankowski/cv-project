e1a820308328809f94423955d555fc11193dcc06
SeregeyIankowski <garret933@gmail.com>
2023-07-09 12:21:39 +0300

    feat: add tableCvProjects as returned value in useCvQuery

--- src/graphql/hooks/useCvQuery.ts
+++ src/graphql/hooks/useCvQuery.ts
@@ -3,6 +3,8 @@ import {CV} from "../queries";
 import {useEffect, useState} from "react";
 import {Cv} from "../interfaces/Cv.interface";
 import {FETCH_POLICY} from "../fetchPolicy";
+import {ProjectTableData} from "@/models/TableDataTypes";
+import {EmptyFieldStrings} from "@/models/emptyFieldsStrings";
 
 const initial: Cv = {
   id: "",
@@ -15,11 +17,33 @@ const initial: Cv = {
   is_template: false,
 };
 
+type convertCvProjectsToTableDataType = (
+  projects: Cv["projects"]
+) => ProjectTableData[];
+const convertCvProjectsToTableData: convertCvProjectsToTableDataType =
+  projects => {
+    if (!projects) return [];
+    return projects.map(project => {
+      return {
+        id: project.id,
+        name: project.name,
+        internalName: project.internal_name,
+        domain: project.domain,
+        startDate: project.start_date,
+        endDate: project?.end_date || EmptyFieldStrings.tillNow,
+        teamSize: project.team_size,
+      };
+    });
+  };
+
 export const useCvQuery = () => {
-  const [loadInfo, {data, loading, error}] = useLazyQuery(CV, {
+  const [loadInfo, {called, data, loading, error}] = useLazyQuery(CV, {
     fetchPolicy: FETCH_POLICY.cacheAndNetwork,
   });
   const [cv, setCv] = useState(initial);
+  const [tableCvProjects, settableCvProjects] = useState<ProjectTableData[]>(
+    []
+  );
 
   const loadCv = (id: Cv["id"]) => {
     return loadInfo({variables: {id}});
@@ -30,8 +54,15 @@ export const useCvQuery = () => {
     if (error) return;
     if (data) {
       setCv(data.cv);
+      settableCvProjects(convertCvProjectsToTableData(data.cv.projects));
     }
   }, [loading, data]);
 
-  return {loadCv, cvData: cv};
+  return {
+    called,
+    loading,
+    loadCv,
+    cvData: cv,
+    tableCvProjects,
+  };
 };
