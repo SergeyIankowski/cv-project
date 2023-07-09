0179de69253837c9ec08a51b11e349f6aa29ec89
SeregeyIankowski <garret933@gmail.com>
2023-07-09 09:20:13 +0300

    refactor: add aliasses to import paths in useUpdateCvMutation

--- src/graphql/hooks/useUpdateCvMutation.ts
+++ src/graphql/hooks/useUpdateCvMutation.ts
@@ -1,12 +1,14 @@
 import {useMutation} from "@apollo/client";
-import {UPDATE_CV} from "../mutations";
+import {UPDATE_CV} from "@/graphql/mutations";
 import {useCallback} from "react";
-import {USER} from "../queries";
-import {Cv} from "../interfaces/Cv.interface";
-import {CvInput} from "../interfaces/CvInput.interface";
+import {USER} from "@/graphql/queries";
+import {Cv} from "@/graphql/interfaces/Cv.interface";
+import {CvInput} from "@/graphql/interfaces/CvInput.interface";
 
 export const useUpdateCvMutation = () => {
-  const [loadData] = useMutation(UPDATE_CV, {refetchQueries: [USER]});
+  const [loadData] = useMutation(UPDATE_CV, {
+    refetchQueries: [USER],
+  });
 
   const updateCv = useCallback(
     async (id: Cv["id"], cv: CvInput) => await loadData({variables: {id, cv}}),
