f608a379496cd8c5aa68df0968209e4a0f614222
SeregeyIankowski <garret933@gmail.com>
2023-07-11 16:40:25 +0300

    feat: create CvPreviewProject section component

---
+++ src/pages/CvProfile/CvPreview/CvPreviewProject/index.tsx
@@ -0,0 +1,74 @@
+import {FC} from "react";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+import {TypographyDetails} from "@view/TypographyDetails";
+import {Project} from "@/graphql/interfaces/Project.interface";
+import {EmptyFieldStrings} from "@/models/emptyFieldsStrings";
+
+interface CvPreviewProjectProps {
+  projectName: Project["name"];
+  startDate: Project["start_date"];
+  endDate: Project["end_date"];
+  domain: Project["domain"];
+  description: Project["description"];
+}
+
+export const CvPreviewProject: FC<CvPreviewProjectProps> = ({
+  projectName,
+  startDate,
+  endDate,
+  domain,
+  description,
+}) => {
+  return (
+    <Paper elevation={5} sx={{m: "10px"}}>
+      <Grid container>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            gap: "10px",
+            p: "10px 10px 0",
+            width: "45%",
+          }}
+        >
+          <TypographyDetails
+            variant="h5"
+            fieldName="Project Name"
+            content={projectName || EmptyFieldStrings.empty}
+          />
+          <Divider flexItem />
+          <TypographyDetails
+            variant="h5"
+            fieldName="Start Date"
+            content={startDate || EmptyFieldStrings.empty}
+          />
+          <Divider flexItem />
+          <TypographyDetails
+            variant="h5"
+            fieldName="End Date"
+            content={endDate || EmptyFieldStrings.tillNow}
+          />
+          <Divider flexItem />
+          <TypographyDetails
+            variant="h5"
+            fieldName="Domain"
+            content={domain || EmptyFieldStrings.empty}
+          />
+        </Box>
+        <Divider orientation="vertical" flexItem />
+        <Box sx={{p: "10px"}}>
+          <TypographyDetails
+            variant="h5"
+            fieldName="Description"
+            content={description || EmptyFieldStrings.empty}
+          />
+        </Box>
+      </Grid>
+    </Paper>
+  );
+};
