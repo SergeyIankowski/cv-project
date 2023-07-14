1845bed6eb93c66d0dc6d8aedee9898f4c78a60e
SeregeyIankowski <garret933@gmail.com>
2023-07-14 10:07:15 +0300

    refactor: change argument type in useUserQuery

--- src/graphql/hooks/useUserQuery.ts
+++ src/graphql/hooks/useUserQuery.ts
@@ -1,14 +1,15 @@
 import {useLazyQuery} from "@apollo/client";
-import {USER} from "../queries";
+import {USER} from "@/graphql/queries";
 import {useCallback} from "react";
-import {FETCH_POLICY} from "../fetchPolicy";
+import {FETCH_POLICY} from "@/graphql/fetchPolicy";
+import {Skill} from "@/graphql/interfaces/Skill.interface";
 
 export const useUserQuery = () => {
   const [loadInfo, {called, loading, data, error}] = useLazyQuery(USER, {
     fetchPolicy: FETCH_POLICY.cacheAndNetwork,
   });
 
-  const loadUserInfo = useCallback((id: string | number) => {
+  const loadUserInfo = useCallback((id: Skill["id"]) => {
     const idAsString = String(id);
     return loadInfo({variables: {id: idAsString}});
   }, []);
