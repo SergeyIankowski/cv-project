313dc2e24603cd2a3b793418f70edabc00bee86d
SergeyIankowski <garret933@gmail.com>
2023-07-18 18:02:19 +0300

    feat: add fetchPolicy and refetchQueries to useUpdateUser

--- src/graphql/hooks/useUpdateUser.ts
+++ src/graphql/hooks/useUpdateUser.ts
@@ -1,8 +1,19 @@
 import {useMutation} from "@apollo/client";
-import {UPDATE_USER} from "../mutations";
+import {UPDATE_USER} from "@/graphql/mutations";
+import {User} from "@/graphql/interfaces/User.interface";
+import {UpdateUserInput} from "@/graphql/interfaces/UpdateUserInput.interface";
+import {USER} from "@/graphql/queries";
+import {MUTATION_FETCH_POLICY} from "@/graphql/fetchPolicy";
 
 export const useUpdateUser = () => {
-  const [updateUser, {data, loading, error}] = useMutation(UPDATE_USER);
+  const [loadData, {data, loading, error}] = useMutation(UPDATE_USER, {
+    fetchPolicy: MUTATION_FETCH_POLICY.networkOnly,
+    refetchQueries: [USER],
+  });
+
+  const updateUser = async (id: User["id"], user: UpdateUserInput) => {
+    await loadData({variables: {id, user}});
+  };
 
   return {updateUser, loading, data, error};
 };
