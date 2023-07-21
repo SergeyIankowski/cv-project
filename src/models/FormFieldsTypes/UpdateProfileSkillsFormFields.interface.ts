a27d8f822e3f78b1f12b839207323e353c44c952
SergeyIankowski <garret933@gmail.com>
2023-07-21 10:29:31 +0300

    feat: change type inUpdateProfileSkillsForm type

--- src/models/FormFieldsTypes/UpdateProfileSkillsFormFields.interface.ts
+++ src/models/FormFieldsTypes/UpdateProfileSkillsFormFields.interface.ts
@@ -1,6 +1,3 @@
 import {SkillMastery} from "@/graphql/interfaces/SkillMastery.interface";
 
-export type UpdateProfileSkillsFormFields = Pick<
-  SkillMastery,
-  "skill_name" | "mastery"
->;
+export type UpdateProfileSkillsFormFields = SkillMastery;
