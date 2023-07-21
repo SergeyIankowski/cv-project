aafc9235e20b8e7765d50a41785c860c9666eb29
SergeyIankowski <garret933@gmail.com>
2023-07-21 10:39:46 +0300

    feat: create prepareUserForUpdatingSkills util function

---
+++ src/utils/prepareUserForUpdatingSkills.ts
@@ -0,0 +1,23 @@
+import {SkillMastery} from "@/graphql/interfaces/SkillMastery.interface";
+import {UpdateUserInput} from "@/graphql/interfaces/UpdateUserInput.interface";
+import {ProfileInput} from "@/graphql/interfaces/ProfileInput.interface";
+import {FetchedUser} from "@/models/FetchedUser.type";
+
+export const prepareUserForUpdatingSkills = (
+  oldUser: FetchedUser,
+  skills: SkillMastery[]
+): UpdateUserInput => {
+  const profile: ProfileInput = {
+    first_name: oldUser.profile.first_name,
+    last_name: oldUser.profile.last_name,
+    skills: skills.map(skill => {
+      return {skill_name: skill.skill_name, mastery: skill.mastery};
+    }),
+    languages: oldUser.profile.languages,
+  };
+  return {
+    profile,
+    departmentId: oldUser.department.id,
+    positionId: oldUser.position.id,
+  };
+};
