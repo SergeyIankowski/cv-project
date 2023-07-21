f1f4847d265610a7df0f9ea0db6f5ef6bd05cc7b
SergeyIankowski <garret933@gmail.com>
2023-07-21 10:30:24 +0300

    feat: add new responce fields to updateUser mutation

--- src/graphql/mutations/updateUser.ts
+++ src/graphql/mutations/updateUser.ts
@@ -4,12 +4,37 @@ export const UPDATE_USER = gql`
   mutation updateUser($id: ID!, $user: UpdateUserInput!) {
     updateUser(id: $id, user: $user) {
       id
+      created_at
       profile {
+        avatar
         first_name
         last_name
+        full_name
+        skills {
+          skill_name
+          mastery
+        }
+        languages {
+          language_name
+          proficiency
+        }
       }
-      department_name
-      position_name
+      cvs {
+        id
+        name
+        description
+        is_template
+      }
+      email
+      department {
+        id
+        name
+      }
+      position {
+        id
+        name
+      }
+      role
     }
   }
 `;
