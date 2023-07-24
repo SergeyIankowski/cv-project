731faff7f4725c0075de963bc992bad983a6987d
SergeyIankowski <garret933@gmail.com>
2023-07-24 11:47:14 +0300

    feat: remove skills, languages and projectsIds fields from CreateCvFormFields.interface

--- src/models/FormFieldsTypes/CreateCvFormFields.interface.ts
+++ src/models/FormFieldsTypes/CreateCvFormFields.interface.ts
@@ -1,13 +1,8 @@
 import {CvInput} from "@/graphql/interfaces/CvInput.interface";
-import {LanguageProficiency} from "@/graphql/interfaces/LanguageProficiency.interface";
-import {SkillMastery} from "@/graphql/interfaces/SkillMastery.interface";
 
 export interface CreateCvFormFields {
   name: CvInput["name"];
   description: CvInput["description"];
   userId?: CvInput["userId"];
-  projectsIds?: CvInput["projectsIds"];
-  skills: SkillMastery[];
-  languages: LanguageProficiency[];
   is_template: CvInput["is_template"];
 }
